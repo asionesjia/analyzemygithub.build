7868cb755baa2a8b310fe5394aea3ad4e6013285
John Smith <asionesjia@gmail.com>
2024-11-08 13:32:58 +0800

    Build

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
-import createJiti from "jiti";
-import {fileURLToPath} from "node:url";
-
-const jiti = createJiti(fileURLToPath(import.meta.url));
-
-jiti("./src/env/server.ts");
-jiti("./src/env/client.ts");
-/** @type {import("next").NextConfig} */
 const config = {};
 
 export default config;
